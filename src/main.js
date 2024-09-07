3c69b2734ac5e558f2efd1b9a6158bd96b7832e8
Maria Kasycheva <167572285+Kasycheva@users.noreply.github.com>
2024-09-07 21:02:25 +0200

    fixed all

--- src/main.js
+++ src/main.js
@@ -2,21 +2,24 @@ import iziToast from 'izitoast';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import 'izitoast/dist/css/iziToast.min.css';
-import { fetchImages } from './js/pixabay-api'; // Исправлено
-import { createCards } from './js/render-functions';
+import { fetchImages } from './js/pixabay-api'; 
+import { createCards } from './js/render-functions'; 
 
 const form = document.querySelector('.form');
 const galleryList = document.querySelector('.gallery-list');
 const loader = document.querySelector('.loader');
+
 const lightBox = new SimpleLightbox('.gallery-list a', {
   captionsData: 'alt',
   captionDelay: 250,
   overlayOpacity: 0.8,
 });
 
-const onSearchSubmit = e => {
+const onSearchSubmit = async e => {
   e.preventDefault();
+
   loader.classList.remove('hidden');
+  galleryList.innerHTML = '';
 
   const value = e.target.elements.inputField.value.toLowerCase().trim();
   if (!value) {
@@ -29,28 +32,26 @@ const onSearchSubmit = e => {
     return;
   }
 
-  fetchImages(value) 
-    .then(img => {
-      if (img.hits.length === 0) {
-        iziToast.error({
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-          position: 'topRight',
-        });
-      } else {
-        createCards(img.hits);
-        lightBox.refresh();
-      }
-    })
-    .catch(err => {
+  try {
+    const img = await fetchImages(value); 
+    if (img.hits.length === 0) {
       iziToast.error({
-        message: `There was an error: ${err.message}. Please try again!`,
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
         position: 'topRight',
       });
-    })
-    .finally(() => {
-      loader.classList.add('hidden');
+    } else {
+      createCards(img.hits);
+      lightBox.refresh();
+    }
+  } catch (err) {
+    iziToast.error({
+      message: `There was an error: ${err.message}. Please try again!`,
+      position: 'topRight',
     });
+  } finally {
+    loader.classList.add('hidden');
+  }
 
   form.reset();
 };
